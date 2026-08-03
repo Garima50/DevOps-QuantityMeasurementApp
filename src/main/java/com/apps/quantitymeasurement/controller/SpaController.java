f3039ec759b8f1f015af19c79e3bc4efcf9bf7ab
Garima50 <garimagoyal705@gmail.com>
2026-08-03 14:40:43 +0530

    fixed redirect url

--- src/main/java/com/apps/quantitymeasurement/controller/SpaController.java
+++ src/main/java/com/apps/quantitymeasurement/controller/SpaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SpaController {
 
     @GetMapping(value = {
+            "/",
             "/{path:^(?!api|swagger-ui|api-docs|h2-console|assets|index\\.html).*$}"
     })
     public String forward() {
